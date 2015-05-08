0ebd35d1de688aca71d3ccd70102be572a832c1b
Jeffrey Wang <jeffw16@users.noreply.github.com>
2015-05-08 13:21:50 -0500

    Making it more Bagely

--- BagelsRunner.java
+++ BagelsRunner.java
@@ -6,8 +6,11 @@ class BagelsRunner {
   }
   public void runBagels () {
     boolean eating = true;
+    boolean samegame = true;
     while ( eating ) {
-      Bagels b = new Bagels ( "123" );
+      Bagels b = new Bagels ( "" + ((int)(Math.random() * (999 - 100) + 100)) );
+      samegame = true;
+      while ( samegame ) {
       System.out.println ( "Try guessing: " );
       Scanner s = new Scanner ( System.in );
       String guess = s.nextLine();
@@ -22,18 +25,21 @@ class BagelsRunner {
           System.out.println ( "Bye!" );
         } else {
           System.out.println ( "Rebooting..." );
+          samegame = false;
         }
       } else {
         System.out.println ( "No, but here are some clues:" );
         System.out.println ( b );
       }
+      }
     }
   }
   public static void main ( String [] args ) {
+      System.out.println ( "\n\n\n\n\n" );
       System.out.println ( "simpleBagels Java" );
       System.out.println ( "by Jeffrey Wang" );
-      System.out.println ( "All guesses are numbers." );
-      System.out.println ( "! means the position is wrong, ? means the number is not within the array." );
+      System.out.println ( "All guesses are 3-digit numbers from 100 to 999." );
+      System.out.println ( "[Fermi] means your number is at the right position, [Pico] means the position is wrong, [Bagels] means the number is not within the guess." );
       BagelsRunner b = new BagelsRunner();
       b.runBagels();
   }
